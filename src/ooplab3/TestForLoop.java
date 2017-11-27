8bf5ab2e4870cecc643890e7ffc0adcd67dc0a1f
newzuz3820 <newzuz3820@gmail.com>
2017-11-27 16:40:14 +0700

    System Error. 3

---
+++ src/ooplab3/TestForLoop.java
@@ -0,0 +1,28 @@
+package ooplab3;
+
+public class TestForLoop {
+    public static void main(String[] args) {
+
+            for (int i =2;i<=100;i++)
+                if (i%3==0||i%5==0||i%7==0)
+                System.out.print(i + " ");
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+
+
+}//calss
