71ddbc70fb40c2eff864e442d3faf9d4f9176bd5
newzuz3820 <newzuz3820@gmail.com>
2017-12-25 16:12:53 +0700

    25122016

---
+++ src/ooplab5/MyArrayList.java
@@ -0,0 +1,28 @@
+package ooplab5;
+
+import java.util.ArrayList;
+
+public class MyArrayList {
+    public static void main(String[] args) {
+        ArrayList myList = new ArrayList();
+        //add method
+        myList.add("BOW");
+        System.out.println(myList);
+        myList.add("AORN");
+        myList.add("NEW");
+        System.out.println(myList);
+        System.out.println(myList.size());
+        myList.add(2,"Hello");
+        System.out.println(myList);
+        myList.add(1,100);
+        System.out.println(myList);
+        myList.set(0,"Narubol");
+        System.out.println(myList);
+        myList.remove("Hello");
+        System.out.println(myList.indexOf(100));
+    }//main
+}//class
+
+
+
+
