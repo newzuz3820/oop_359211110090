d01747046476f8d740b1b88d929846d4f5dccb58
LABCOM_6 <newzuz3820@gmail.com>
2018-02-19 14:23:05 +0700

    Campus

---
+++ src/ooplab8/Saiyai.java
@@ -0,0 +1,13 @@
+package ooplab8;
+
+public class Saiyai implements Campus {
+
+    public void getCampusName() {
+        System.out.println("Saiyai Campus.");
+    }
+
+    public void getArea() {
+        System.out.println("Thugsong Nakhonsi thammarat.");
+
+    }
+}
